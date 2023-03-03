727ce1744a6fd44a62e0c3e7cc48af0be6dff164
Rageon <vnifedow@bk.ru>
2023-03-03 19:34:35 +0500

    Blowfish implementation

---
+++ _Encrypt_Lab2/Blowfish.cs
@@ -0,0 +1,53 @@
+﻿using Simias.Encryption;
+using System.Text;
+
+namespace _Encrypt_Lab2
+{
+    internal class Blowfish
+    {
+
+        byte[] GlobalKey = null!;
+
+        public void DoStuff(bool sslGenerated)
+        {
+            Console.Clear();
+            byte[] key = Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw==");
+            Console.WriteLine("1 - Зашифровать\n2 - Расшифровать");
+            var selector = Convert.ToInt32(Console.ReadLine());
+            switch(selector)
+            {
+                case 1:
+                    if (sslGenerated)
+                    {
+                        if (GlobalKey == null)
+                        {
+                            GlobalKey = SSL.GenerateKey();
+                        }
+                        Console.WriteLine("Ключ: " + Convert.ToBase64String(GlobalKey));
+                    }
+                    Console.WriteLine("Введите строку для шифрования");
+                    string toEncrypt = Console.ReadLine()!;
+                    File.WriteAllBytes("blowfish_encrypted", Encrypt(toEncrypt, sslGenerated ? GlobalKey : key));
+                    break;
+                case 2:
+                    byte[] encrypted = File.ReadAllBytes("blowfish_encrypted");
+                    Decrypt(encrypted, sslGenerated ? GlobalKey : key);
+                    break;
+            }
+            Console.ReadKey();
+        }
+        byte[] Encrypt(string toEncrypt, byte[] key)
+        {
+            BlowfishCore blowfishCore = new(key);
+            byte[] encrypted = blowfishCore.Encipher(ASCIIEncoding.ASCII.GetBytes(toEncrypt), (byte)8);
+            Console.WriteLine(toEncrypt + " -> " + Convert.ToBase64String(encrypted));
+            return encrypted;
+        }
+        void Decrypt(byte[] encrypted, byte[] key)
+        {
+            BlowfishCore blowfishCore = new(key);
+            string decrypted = ASCIIEncoding.ASCII.GetString(blowfishCore.Decipher(encrypted, (byte)8));
+            Console.WriteLine(Convert.ToBase64String(encrypted) + " -> " + decrypted);
+        }
+    }
+}
--- _Encrypt_Lab2/Program.cs
+++ _Encrypt_Lab2/Program.cs
@@ -1,27 +1,26 @@
-﻿using System.Text;
-
-namespace _Encrypt_Lab2 {
+﻿namespace _Encrypt_Lab2 {
     static class Program
     {
 
         static AES aes = new();
+        static Blowfish blowfish = new();
 
         static void Main()
         {
             try 
             {
                 Console.Clear();
-                Console.WriteLine("Выберите метод шифрования\n1 - AES");
+                Console.WriteLine("Выберите метод шифрования\n1 - AES\n2 - Blowjob");
                 var selector = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ключ генерит SSL?\n1 - Нет\n2 - Да");
+                var selectorSsl = Convert.ToInt32(Console.ReadLine());
                 switch (selector)
                 {
                     case 1:
-                        Console.WriteLine("Ключ генерит SSL?\n1 - Нет\n2 - Да");
-                        selector = Convert.ToInt32(Console.ReadLine());
-                        aes.AesEncryption(Convert.ToBoolean(selector - 1));
+                        aes.AesEncryption(Convert.ToBoolean(selectorSsl - 1));
                         break;
                     case 2:
-
+                        blowfish.DoStuff(Convert.ToBoolean(selectorSsl - 1));
                         break;
                 }
             }
