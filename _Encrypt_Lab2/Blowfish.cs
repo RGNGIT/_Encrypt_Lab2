28a145a26a1ada8acf09e720e4761778e597ebb8
RGNGIT <vnifedow@bk.ru>
2023-03-04 20:17:38 +0500

    +++

--- _Encrypt_Lab2/Blowfish.cs
+++ _Encrypt_Lab2/Blowfish.cs
@@ -8,6 +8,37 @@ namespace _Encrypt_Lab2
 
         byte[]? GlobalKey = null;
 
+        public void ImageEncryption(string name, bool sslGenerated) 
+        {
+            Console.Clear();
+            Console.WriteLine("1 - Зашифровать\n2 - Расшифровать");
+            byte[] key = Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw==");
+            if (sslGenerated)
+            {
+                // Сгенерированный симметричный ключ
+                if (GlobalKey == null) GlobalKey = SSL.GenerateKey();
+                Console.WriteLine("Ключ: " + Convert.ToBase64String(GlobalKey));
+            }
+            var selector = Convert.ToInt32(Console.ReadLine());
+            switch(selector)
+            {
+                case 1:
+                    byte[] image = File.ReadAllBytes(name);
+                    string base64Image = Convert.ToBase64String(image);
+                    byte[] result = Encrypt(base64Image, sslGenerated ? GlobalKey! : key);
+                    Console.WriteLine(Convert.ToBase64String(result));
+                    File.WriteAllBytes("aes_encrypted_img", result);
+                    break;
+                case 2:
+                    byte[] toDecrypt = File.ReadAllBytes("aes_encrypted_img");
+                    string base64Decrypt = Decrypt(toDecrypt, sslGenerated ? GlobalKey! : key);
+                    Console.WriteLine(base64Decrypt);
+                    File.WriteAllBytes(name, Convert.FromBase64String(base64Decrypt));
+                    break;
+            }
+            Console.ReadKey();
+        }
+
         public void DoStuff(bool sslGenerated)
         {
             Console.Clear();
@@ -40,11 +71,12 @@ namespace _Encrypt_Lab2
             Console.WriteLine(toEncrypt + " -> " + Convert.ToBase64String(encrypted));
             return encrypted;
         }
-        void Decrypt(byte[] encrypted, byte[] key)
+        string Decrypt(byte[] encrypted, byte[] key)
         {
             BlowfishCore blowfishCore = new(key);
             string decrypted = ASCIIEncoding.ASCII.GetString(blowfishCore.Decipher(encrypted, (byte)8));
             Console.WriteLine(Convert.ToBase64String(encrypted) + " -> " + decrypted);
+            return decrypted;
         }
     }
 }
